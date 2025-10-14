94a535fe864032c4c80547612fa8e7ccca3ac840
mattrod30 <matthewrodriguez1430@gmail.com>
2025-10-14 15:58:22 -0700

    Update MinMax.cpp

--- MinMax.cpp
+++ MinMax.cpp
@@ -4,15 +4,18 @@
 using namespace std;
 
 int maxInteger(int a[], int size);
+int minInteger(int a[], int size);
+
 
 int main() {
 	const int size = 20;
 	int randomNumbers[size];
 	srand(time(0));
-	
+
 	for (int i = 0; i < size; i++) {
 		randomNumbers[i] = rand() % 100 + 1;
-
+		int maxInt = maxInteger(randomNumbers, size);
+		int minInt = minInteger(randomNumbers, size);
 	}
 
 
@@ -21,8 +24,23 @@ int main() {
 	return 0;
 }
 int maxInteger(int a[], int size) {
-	int min = a[0];
 	int max = a[0];
+	for (int i = 1; i < size; i++) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+	return max;
 
-	return 0;
 }
+int minInteger(int a[], int size) {
+	int min = a[0];
+	for (int i = 1; i < size; i++) {
+		if (a[i] > min) {
+			min = a[i];
+		}
+	}
+	return min;
+}
+
+
