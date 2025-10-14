e218db931f256e3ee03fa42aee06dfe9d25945ef
mattrod30 <matthewrodriguez1430@gmail.com>
2025-10-13 17:53:29 -0700

    Add files via upload

---
+++ madlib.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool runAgain(void);
+
+int main() {
+	srand(time(0));
+	const int size = 3;
+
+	string adverb[size] = {"weirdly", "dangerously", "ominously" };
+	string adjectives[size] = { "spectacular", "patient," "dorky" };
+	string nouns[size] = { "schoool", "disney land," "dorky" };
+	string verbs[size] = { "spectacular", "patient," "dorky" };
+	string subject[size] = { "spectacular", "patient," "dorky" };
+
+
+
+
+	
+
+
+
+	return 0;
+}
