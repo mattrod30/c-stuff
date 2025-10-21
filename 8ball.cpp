18ea42f75674a9e8b9ec1719b02a0222dfb89b8d
mattrod30 <matthewrodriguez1430@gmail.com>
2025-10-20 18:29:28 -0700

    Update 8ball.cpp

--- 8ball.cpp
+++ 8ball.cpp
@@ -9,18 +9,22 @@ bool runAgain(void);
 int main() {
     srand(time(0));
 
-    string userResp;
     const int size = 20;
-    string a[size] = { "It is certain.", "Reply hazy, try again.", "Don't count on it." };
+    string a[size] = { "It is certain.", "As I see it, yes.", "Reply hazy, try again.",  "Don't count on it." ,
+    "It is decidedly so.", "Without a doubt.", "Yes definitely.",
+    "You may rely on it.", "Most likely.", "Outlook good.","Yes.", "Signs point to yes.", "Ask again later.", 
+    "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
+    "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful." };
 
 
-    cout << "\n Magic 8-Ball App. (@ Mattel)\n" << endl;
+    cout << "\nMagic 8-Ball App. (@ Mattel)\n" << endl;
 
     do {
-        cout << "Ask me anything!" << endl;
+        string userResp;
+        cout << "\nAsk me anything! ";
         getline(cin, userResp);
-       
-        cout << "\nMagic 8-ball says: " << a[rand() % size] << endl;
+
+        cout << "Magic 8-ball says: " << a[rand() % size] << endl;
 
     } while (runAgain());
 
@@ -29,8 +33,9 @@ int main() {
 
 bool runAgain() {
     char userResponse;
-    cout << "Would you like to run again? (y or n): ";
+    cout << "\nAsk Magic 8-ball again? (y or n): ";
     cin >> userResponse;
+    cin.ignore();
 
     return (userResponse == 'y' || userResponse == 'Y');
 }
