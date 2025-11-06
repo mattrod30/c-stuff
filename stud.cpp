6aea067507ebaf4610262c7f207f747866def379
mattrod30 <matthewrodriguez1430@gmail.com>
2025-11-06 00:28:47 -0600

    Update stud.cpp

--- stud.cpp
+++ stud.cpp
@@ -1,74 +1,56 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 struct Student {
     string firstName;
     string lastName;
     string school;
-    string ID;
     float GPA;
-
-    void capitalizeName(void);
-    void print(void);
-    char grade(void);
 };
 
+void capitalize(Student& arg);
+void printStudent(Student arg);
 
-int main(void) {
+int main() {
+    Student s;
 
-    const int size = 3;
-    Student studentList[size];  
+    cout << "Enter first name: ";
+    getline(cin, s.firstName);
+    cout << "Enter last name: ";
+    getline(cin, s.lastName);
+    cout << "Enter school: ";
+    getline(cin, s.school);
+    cout << "Enter GPA: ";
+    cin >> s.GPA;
 
+    capitalize(s);
 
-    for (int i = 0; i < size; i++) {
-        cout << "Enter student first name: ";
-        getline(cin, studentList[i].firstName);
-        cout << "Enter student last name: ";
-        getline(cin, studentList[i].lastName);
-        cout << "Enter school: ";
-        getline(cin, studentList[i].school);
-        cout << "Enter school ID: ";
-        getline(cin, studentList[i].ID);
-        cout << "Enter GPA: ";
-        cin >> studentList[i].GPA;
-        cin.ignore();
-        studentList[i].capitalizeName();
-        cout << endl;
-    }
-
-    
-    cout << "\nList of all students in the array. \n" << endl;
-    for (int i = 0; i < size; i++) {
-        studentList[i].print();
-        cout << "\n***\n" << endl;
-    }
+    cout << "\n--- Student Information ---\n";
+    printStudent(s);
 
     return 0;
 }
 
-void Student::capitalizeName(void) {
-    if (!firstName.empty())
-        firstName[0] = toupper(firstName[0]);
-    if (!lastName.empty())
-        lastName[0] = toupper(lastName[0]);
+void capitalize(Student& arg) {
+    if (!arg.firstName.empty()) {
+        arg.firstName[0] = toupper(arg.firstName[0]);
+        for (size_t i = 1; i < arg.firstName.size(); ++i)
+            arg.firstName[i] = tolower(arg.firstName[i]);
+    }
+    if (!arg.lastName.empty()) {
+        arg.lastName[0] = toupper(arg.lastName[0]);
+        for (size_t i = 1; i < arg.lastName.size(); ++i)
+            arg.lastName[i] = tolower(arg.lastName[i]);
+    }
 }
 
+void printStudent(Student arg) {
+    cout << "First name: " << arg.firstName << '\n';
+    cout << "Last name: " << arg.lastName << '\n';
+    cout << "School: " << arg.school << '\n';
+    cout << fixed << setprecision(2);
+    cout << "GPA: " << arg.GPA << "\n";
+}
 
-char Student::grade(void) {
-    if (GPA >= 3.5)
-        return 'A';
-    else if (GPA >= 3.0)
-        return 'B';
-    else if (GPA >= 2.0)
-        return 'C';
-    else if (GPA >= 1.0)
-        return 'D';
-    else
-        return 'F';
-}
-void Student::print(void) {
-    cout << "Student name: " << firstName << " " << lastName << endl;
-    cout << "Attends " << school << " with GPA: " << GPA << endl;
-    cout << "ID: " << ID << " with grade: " << grade() << endl;
-}
