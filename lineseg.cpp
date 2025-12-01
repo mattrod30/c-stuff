2d5f5b962bb8954dc63d2901f6564d77f6f6775a
mattrod30 <matthewrodriguez1430@gmail.com>
2025-12-01 15:54:49 -0800

    Create lineseg.cpp

---
+++ lineseg.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+struct phone{
+private:
+
+  int battery, storage
+  string model;
+
+public(int batt = 65, int stor = 32, string m = iphone 16){
+batt = battery;
+stor = storage
+m = model;
+
+getModel(const{return model;}
+getBattery(const{return battery;}
+getStorage(const{return storage;}
+
+
+setStorage(string Newstorage){
+if(newStorage >= 0)
+newStorage = storage
+
+}
+setBattery(string Newbattery){
+if (newBattery < 0)
+newBattery = 0;
+if (newBattery > 100)
+newBattery = 100;
+
+
+};
