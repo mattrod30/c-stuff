116efe9f39deebb62c8175db4ab4f8380aadde55
mattrod30 <matthewrodriguez1430@gmail.com>
2026-01-21 16:40:39 -0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -19,56 +19,67 @@ int main() {
 	Student <double> x;
 	string studentName;
 	int studentID;
-	double examAvg,homeworkAvg, finalExamAvg;
+	double examAvg, homeworkAvg, finalExamAvg;
 	float examWeight, homeworkWeight, finalExamWeight;
 
-	cout << "Enter the student's name";
+	cout << "Enter the student's name" << endl;
 	cin >> studentName;
 	x.name = studentName;
 
-
-	cout << "Enter the student's ID";
+	cout << "Enter the student's ID" << endl;
 	cin >> studentID;
 	x.ID = studentID;
 
-	cout << "Enter the student's test average";
+	cout << "Enter the student's test average" << endl;
 	cin >> examAvg;
 	x.testAvg = examAvg;
 
-	cout << "Enter the student's homework average";
+	cout << "Enter the student's homework average" << endl;
 	cin >> homeworkAvg;
-	x.testAvg = homeworkAvg;
+	x.hwAvg = homeworkAvg;
 
-	cout << "Enter the student's final exam average";
+	cout << "Enter the student's final exam average" << endl;
 	cin >> finalExamAvg;
-	x.testAvg = finalExamAvg;
+	x.finalAvg = finalExamAvg;
 
-	cout << "Enter the student's test weight";
+	cout << "Enter the student's test weight" << endl;
 	cin >> examWeight;
-	x.testAvg = examWeight;
+	x.testWeight = examWeight;
 
-	cout << "Enter the student's homework weight";
+	cout << "Enter the student's homework weight" << endl;
 	cin >> homeworkWeight;
-	x.testAvg = homeworkWeight;
+	x.hwWeight = homeworkWeight;
 
-	cout << "Enter the student's final exam weight";
+	cout << "Enter the student's final exam weight" << endl;
 	cin >> finalExamWeight;
-	x.testAvg = finalExamWeight;
+	x.finalWeight = finalExamWeight;
 
+	return (0);
 }
+
 template<class T>
 void Student<T>::display(void) {
-	cout <<
+	cout << "Student name: " << name << endl;
+	cout << "Student ID: " << ID << endl;
+	cout << "Student test average: " << testAvg << endl;
+	cout << "Student homework average: : " << hwAvg << endl;
+	cout << "Student final exam average: " << finalExamAvg << endl;
+	cout << "Exam weight: " << testWeight << endl;
+	cout << "Homework weight : " << hwWeight << endl;
+	cout << "Final exam weight: " << finalWeight << endl;
+
+
+
 }
 
 
 template<class T>
 double Student<T>::calculateWeightedAverage(void) {
-	return 
+	return 42
 }
 
 
 template<class T>
 char Student<T>::getGrade(void) {
-	return
+	return 42
 }
