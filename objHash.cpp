980a2a66a000171b2e1af755b0359f8de118822f
mattrod30 <matthewrodriguez1430@gmail.com>
2026-04-20 11:49:25 -0700

    Update objHash.cpp

--- objHash.cpp
+++ objHash.cpp
@@ -1,41 +1,65 @@
 #include <iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
-class Person{
+class Person {
 private:
     string name;
     int age;
-    
+
 public:
-    Person(string argN = "Matt", int argAge = 20){
+    Person(string argN = "Matt", int argAge = 20) {
         name = argN;
         age = argAge;
     }
-    
+
+    string getName();
+    int getAge();
 };
 
-class Student:public Person{
-    private:
+class Student :public Person {
+private:
     int ID;
 public:
-    Student(int argID = 01234){
+    Student(string argN = "Matt", int argAge = 20, int argID = 1234)
+        : Person(argN, argAge) {
         ID = argID;
     }
+
+    int getID();
+    void display();
 };
 int stringHash(string name, int buckets);
 
-int main(){
-    
-    
-    
+int main() {
+    int buckets = 5;
+
+
     return 0;
 }
 
-int stringHash(string name, int buckets){
+int stringHash(string name, int buckets) {
     int sum = 0;
-        for (int i = 0; i < name.size(); i++) {
-            sum += int(name[i]);
-        }
-        return sum % buckets;
+    for (int i = 0; i < name.size(); i++) {
+        sum += int(name[i]);
     }
+    return sum % buckets;
+}
+
+string Person::getName() {
+    return name;
+}
+
+int Person::getAge() {
+    return age;
+}
+
+int Student::getID() {
+    return ID;
+}
+
+void Student::display() {
+    cout << "Name: " << getName() << " | Age: " << getAge() << " | ID: " << getID << endl;
+}
+
